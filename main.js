ffd017f27eda4ac21c5f181e0340f1fefce66d98
gjspencer22 <gjspencer22@gmail.com>
2022-01-02 23:25:36 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,22 @@
+import './style.css'
+
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyAI41IeCY1yLDWkZvEabSfsesuIgMILjjE",
+
+  authDomain: "video-chat-46631.firebaseapp.com",
+
+  projectId: "video-chat-46631",
+
+  storageBucket: "video-chat-46631.appspot.com",
+
+  messagingSenderId: "466173515901",
+
+  appId: "1:466173515901:web:8b2d22e294029d4d2b3914",
+
+  measurementId: "G-R38MHGMG06"
+
+};
